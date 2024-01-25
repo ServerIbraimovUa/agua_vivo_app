e4add0a81412feee7cd68e5b2e64953aea8b24e9
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2024-01-25 23:13:11 +0200

    added authForm

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,14 @@
+import AuthForm from "../components/AuthForm/AuthForm";
+import { NavLink } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <AuthForm />
+      <NavLink to="/signup">Sign up</NavLink>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,16 @@
+import { NavLink } from "react-router-dom";
+import AuthForm from "../components/AuthForm/AuthForm";
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <div>
+        <h2>Sign In</h2>
+        <AuthForm />
+        <NavLink to="/signin">Sign in</NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
