cc19ca69b62af6503a190d2cef91f0f6455cdea2
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2024-01-25 19:59:37 +0200

    deleted link

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,14 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-// import Header from "../Header/Header";
+import Header from "../Header/Header";
 import { Suspense } from "react";
 import Loading from "../Loading/Loading";
 
 const Layout = () => {
   return (
     <>
-      {/* <Header /> */}
-      <header>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/welcome">Welcome</NavLink>
-        <NavLink to="/signup">Sign up</NavLink>
-        <NavLink to="/signin">Sign in</NavLink>
-      </header>
+      <Header />
+
       <main>
         {
           <Suspense fallback={<Loading />}>
