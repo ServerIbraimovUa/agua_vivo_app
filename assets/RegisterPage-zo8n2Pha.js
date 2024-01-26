203768eaae6cb7aa9a68c4e95fc59d8db8f79d79
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2024-01-26 20:02:27 +0000

    Deploying to gh-pages from @ ServerIbraimovUa/agua_vivo_app@8826df2c51034d7bb29f1f54ee1a56e59ec5fa26 🚀

---
+++ assets/RegisterPage-zo8n2Pha.js
@@ -0,0 +1,1 @@
+import{j as i,N as r}from"./index-Fk_bWuy9.js";import{A as s}from"./AuthForm-jQeT8jC6.js";const t=()=>i.jsx("div",{children:i.jsxs("div",{children:[i.jsx("h2",{children:"Sign Up"}),i.jsx(s,{repeat:!0}),i.jsx(r,{to:"/signin",children:"Sign in"})]})});export{t as default};
