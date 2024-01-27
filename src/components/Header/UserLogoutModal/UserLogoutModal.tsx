7f4f27519d30f69150a78126e3992104398465fd
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-27 10:30:49 +0200

    added UserLogoutModal

---
+++ src/components/Header/UserLogoutModal/UserLogoutModal.tsx
@@ -0,0 +1,16 @@
+const UserLogoutModal = () => {
+  return (
+    <div>
+      <h2>Log out</h2>
+
+      <p>Do you really want to leave?</p>
+
+      <div>
+        <button type="button">Cancel</button>
+        <button type="submit">Log out</button>
+      </div>
+    </div>
+  );
+};
+
+export default UserLogoutModal;
