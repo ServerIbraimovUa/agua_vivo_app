a46d0e01aab15a24f5a514abc101b80ebf793300
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-27 10:31:15 +0200

    added SettingModal

---
+++ src/components/Header/SettingModal/SettingModal.tsx
@@ -0,0 +1,136 @@
+import { useForm } from "react-hook-form";
+// import { FC } from "react";
+
+interface SettingForm {
+  // photo:
+  gender?: "woman" | "man";
+  name: string;
+  email: string;
+  outdatedPassword: string;
+  newPassword: string;
+  repeatNewPassword: string;
+}
+
+const SettingModal = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+
+    formState: { errors },
+  } = useForm<SettingForm>();
+
+  const onSubmit = (data: SettingForm) => {
+    console.log(data);
+
+    reset();
+  };
+
+  return (
+    <div>
+      <h2>Setting</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          {/* <label>
+            <img></img>
+            <span>Your photo</span>
+            <input type="file" name="remember" />
+            <span>Upload a photo</span>
+          </label> */}
+
+          <span>Your gender identity</span>
+
+          <div>
+            <label>
+              <input
+                {...register("gender", { required: true })}
+                value="woman"
+                type="radio"
+              />
+              <span>Woman</span>
+            </label>
+            <label>
+              <input
+                {...register("gender", { required: true })}
+                value="man"
+                type="radio"
+              />
+              <span>Man</span>
+            </label>
+          </div>
+
+          <label>
+            <span>Your name</span>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              placeholder="name"
+            />
+            {errors.name && <span>This field is required</span>}
+          </label>
+
+          <label>
+            <span>E-mail</span>
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              placeholder="email"
+            />
+            {errors.email && <span>This field is required</span>}
+          </label>
+        </div>
+
+        <div>
+          <span>Password</span>
+
+          <label>
+            <span>Outdated password:</span>
+            <input
+              {...register("outdatedPassword", {
+                required: true,
+                minLength: 8,
+                maxLength: 64,
+              })}
+              type="password"
+              placeholder="Password"
+            />
+            {errors.outdatedPassword && <span>This field is required</span>}
+          </label>
+
+          <label>
+            <span>New Password:</span>
+            <input
+              {...register("newPassword", {
+                required: true,
+                minLength: 8,
+                maxLength: 64,
+              })}
+              type="password"
+              placeholder="Password"
+            />
+            {errors.newPassword && <span>This field is required</span>}
+          </label>
+
+          <label>
+            <span>Repeat new password:</span>
+            <input
+              {...register("repeatNewPassword", {
+                required: true,
+                minLength: 8,
+                maxLength: 64,
+              })}
+              type="password"
+              placeholder="Password"
+            />
+            {errors.repeatNewPassword && <span>This field is required</span>}
+          </label>
+        </div>
+
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default SettingModal;
