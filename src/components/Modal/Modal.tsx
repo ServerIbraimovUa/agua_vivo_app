2f6e8a8abfeb7d5e74604b26d328c2f34f09d86e
Oksana-Tnt <oksanatantsiura@gmail.com>
2024-01-28 12:26:15 +0100

    create component DailyNorma

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect } from "react";
 import { Overlay } from "./Modal.styled";
 import Icon from "../Icon/Icon";
 
@@ -8,9 +8,31 @@ interface Props {
   children: ReactNode;
 }
 
+
+
 const Modal: FC<Props> = ({ setVisible, title, children }) => {
+
+  useEffect(() => {
+    const handleKeyDown = (e:KeyboardEvent) => {
+      if (e.code === 'Escape') {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [setVisible]);
+
+  const handleBackdropClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.currentTarget === e.target) {
+      setVisible(false);
+    }
+  };
+
   return (
-    <Overlay>
+    <Overlay onClick={handleBackdropClick}>
       <div className="modal">
         <div className="header">
           <p>{title}</p>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import DailyNorma from "../components/DailyNorma/DailyNorma";
 import WaterList from "../components/WaterList/WaterList";
 
 const HomePage = () => {
   return (
     <section>
       <div className="container">
+        <DailyNorma/>
         <WaterList />
       </div>
     </section>
