2688accfe3254b75a74f5feb6d86e89bbabea1a5
AnastasiiaDia <anastasiia.iv@gmail.com>
2024-01-29 20:36:11 +0100

    update welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,34 +1,20 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { Navigate, redirect } from "react-router-dom";
-import WelcomeList from "../components/WelcomeList/WelcomeList";
-=======
 import { useNavigate } from "react-router-dom";
 import WhyDrinkWater from "../components/WhyDrinkWater/WhyDrinkWater";
 import WaterConsumptionTracker from "../components/WaterConsumptionTracker/WaterConsumptionTracker";
-import { Section } from "../components/WhyDrinkWater/WhyDrinkWaterStyled.styled";
 
->>>>>>> Stashed changes
 const WelcomePage = () => {
+  const navigate = useNavigate();
   const handleClick = () => {
-    return <Navigate to="/signup" />;
+    return navigate("/signup");
   };
 
   return (
-    <Section>
-      <div className="container welcome-container">
+    <section>
+      <div className="container">
         <WaterConsumptionTracker handleClick={handleClick} />
         <WhyDrinkWater />
       </div>
-<<<<<<< Updated upstream
-      <div>
-        <h3>WhyDrinkWater</h3>
-        <WelcomeList />
-      </div>
     </section>
-=======
-    </Section>
->>>>>>> Stashed changes
   );
 };
 
