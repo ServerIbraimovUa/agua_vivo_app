c4acbe746982ee2b31c264104318a2dd39cad525
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-29 14:23:25 +0200

    added avatar on settingModal

---
+++ src/components/Header/SettingModal/SettingModal.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FormSettingStyled = styled.form`
+  .avatar {
+    width: 80px;
+    height: 80px;
+  }
+`;
+
+// .modal-icon {
+//         width: 12px;
+//         height: 12px;
+//         stroke: #407bff;
+//       }
