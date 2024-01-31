1423cd115dc493e6298e110bef7d7fb1b769ed78
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2024-01-31 13:20:42 +0000

    Deploying to gh-pages from @ ServerIbraimovUa/agua_vivo_app@747ec5719a54ee0bc010fd1a7cc44e98069694ab 🚀

--- assets/WelcomePage-yucPLTrf.js
+++ assets/WelcomePage-yucPLTrf.js
@@ -1,4 +1,4 @@
-import{u as t,d as i,j as e,I as s,w as a}from"./index-hTCSDsgi.js";const o=t.ul`
+import{u as t,d as i,j as e,I as s,x as a}from"./index-1u4TPV9L.js";const o=t.ul`
   list-style: none;
   display: flex;
   flex-direction: column;
