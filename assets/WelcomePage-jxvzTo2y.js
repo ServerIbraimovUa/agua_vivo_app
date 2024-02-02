7fe82c053c81709f86c9de89bc84eb81f135b9de
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2024-02-02 07:00:45 +0000

    Deploying to gh-pages from @ ServerIbraimovUa/agua_vivo_app@cb8f3b73ef7ff925375cb1c5237df6898d9b14dc 🚀

--- assets/WelcomePage-jxvzTo2y.js
+++ assets/WelcomePage-jxvzTo2y.js
@@ -1,4 +1,4 @@
-import{u as t,d as i,j as e,I as s,w as a}from"./index-o6ybWq9h.js";const o=t.ul`
+import{u as t,d as i,j as e,I as s,w as a}from"./index-rhkdOiWc.js";const o=t.ul`
   list-style: none;
   display: flex;
   flex-direction: column;
