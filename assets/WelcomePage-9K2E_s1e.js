015eb9f76fc8359a4a98fb3508f5eb4c0cc0489c
Viktoriia-Ch08 <112558987+Viktoriia-Ch08@users.noreply.github.com>
2024-02-03 12:48:02 +0000

    Deploying to gh-pages from @ ServerIbraimovUa/agua_vivo_app@3e450b65980e9d5dbf84c0ef39f669d1fb00c3a7 🚀

--- assets/WelcomePage-9K2E_s1e.js
+++ assets/WelcomePage-9K2E_s1e.js
@@ -1,4 +1,4 @@
-import{u as t,d as i,j as e,I as s,y as o}from"./index-d0P6JViz.js";const l="/agua_vivo_app/assets/welcomeMob-9xutC1rA.png",r="/agua_vivo_app/assets/WelcomeTab-KNRMQHDm.png",c="/agua_vivo_app/assets/WelcomeDesk-FJOaIO84.png",d=t.section`
+import{u as t,d as i,j as e,I as s,y as o}from"./index-0QX9eJni.js";const l="/agua_vivo_app/assets/welcomeMob-9xutC1rA.png",r="/agua_vivo_app/assets/WelcomeTab-KNRMQHDm.png",c="/agua_vivo_app/assets/WelcomeDesk-FJOaIO84.png",d=t.section`
   &.welcome-section {
     background-image: url(${l});
     background-position: center;
